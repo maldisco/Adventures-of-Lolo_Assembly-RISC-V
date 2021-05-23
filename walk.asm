f374199e75a4220a008e0948535f21af34b29781
Filipe <44625571+maldisco@users.noreply.github.com>
2021-05-23 02:46:40 -0300

    Update walk.asm

--- walk.asm
+++ walk.asm
@@ -6,8 +6,8 @@ LOLO_POSY:		.word 36
 LOLO_WALK:
 	li t1, UP 				# t1 = 'W'
 	li t2, DOWN				# t2 = 'S'
-	li t3, RIGHT				# t1 = 'D'
-	li t4, LEFT				# t2 = 'A'
+	li t3, RIGHT				# t3 = 'D'
+	li t4, LEFT				# t4 = 'A'
 	
 	beq s11,t1,LOLO_WALK_UP			# s11 = tecla digitada
 	beq s11,t2,LOLO_WALK_DOWN
@@ -20,17 +20,17 @@ LOLO_WALK_UP:
 	switch_frame()			
 	loadw(a1,LOLO_POSY)
 	addi a1,a1,-16
-	jal IS_DOOR				# Testa se o destino È uma porta
+	jal IS_DOOR				# Testa se o destino √© uma porta
 	li t2, MAP_UPPER_EDGE	
 	blt a1,t2,LWU_INVALID_2			# Testa se Lolo saiu do mapa ( Y maior que borda superior do mapa)
 		loadw(t3,LOLO_POSX)
 		calculate_block(t3,a1)
-		jal IS_MORTAL_BLOCK		# Testa se o bloco destino È um bloco mortal
+		jal IS_MORTAL_BLOCK		# Testa se o bloco destino √© um bloco mortal
 		la t2, WALKABLE_BLOCKS
 		add t3,t2,t1
 		lb t3,(t3)
-		bgtz t3, LWU_INVALID 		# Testa se È um bloco and·vel
-			savew(a1,LOLO_POSY)	# Atualiza a posiÁ„o de Lolo para a prÛxima renderizaÁ„o
+		bgtz t3, LWU_INVALID 		# Testa se √© um bloco and√°vel
+			savew(a1,LOLO_POSY)	# Atualiza a posi√ß√£o de Lolo para a pr√≥xima renderiza√ß√£o
 			jal IS_KEY_BLOCK
 			j LWU_INVALID_2
 	LWU_INVALID:
@@ -48,16 +48,16 @@ LOLO_WALK_DOWN:
 	loadw(a1,LOLO_POSY)
 	addi a1,a1,16
 	li t2, MAP_LOWER_EDGE
-	bgt a1,t2,LWD_INVALID_2 			# Teste est· no mapa
+	bgt a1,t2,LWD_INVALID_2 			# Teste est√° no mapa
 		loadw(t3,LOLO_POSX)
 		calculate_block(t3,a1)
-		jal IS_MORTAL_BLOCK		# Testa se È um bloco mortal
+		jal IS_MORTAL_BLOCK		# Testa se √© um bloco mortal
 		LWD_NOT_MORTAL:		
 		la t2, WALKABLE_BLOCKS
 		add t3,t2,t1
 		lb t3,(t3)
 		bgtz t3,LWD_INVALID
-			savew(a1,LOLO_POSY)	# Atualiza a posiÁ„o de Lolo para a prÛxima renderizaÁ„o
+			savew(a1,LOLO_POSY)	# Atualiza a posi√ß√£o de Lolo para a pr√≥xima renderiza√ß√£o
 			jal IS_KEY_BLOCK
 			j LWD_INVALID_2
 	LWD_INVALID:
@@ -65,7 +65,7 @@ LOLO_WALK_DOWN:
 	beqz t3,LWD_INVALID_2
 		jal PUSHABLE_DOWN
 	LWD_INVALID_2:
-	jal COLISION_TEST			# Testa colis„o com inimigo
+	jal COLISION_TEST			# Testa colis√£o com inimigo
 	render_sprite( lolo_down_1, LOLO_POSX, LOLO_POSY)					
 	frame_refresh()				
 	j POLL_LOOP
@@ -75,16 +75,16 @@ LOLO_WALK_RIGHT:
 	loadw( a1,LOLO_POSX )
 	addi a1,a1,16
 	li t2, MAP_RIGHT_EDGE
-	bgt a1,t2,LWR_INVALID_2			# Testa se est· no mapa
+	bgt a1,t2,LWR_INVALID_2			# Testa se est√° no mapa
 		loadw( t3,LOLO_POSY )
 		calculate_block( a1,t3 )
-		jal IS_MORTAL_BLOCK		# Testa se È um bloco mortal
+		jal IS_MORTAL_BLOCK		# Testa se √© um bloco mortal
 		LWR_NOT_MORTAL:
 		la t2, WALKABLE_BLOCKS
 		add t3,t2,t1
 		lb t3,(t3)
-		bgtz t3,LWR_INVALID		# Testa se È um bloco and·vel
-			savew(a1,LOLO_POSX)	# Atualiza a posiÁ„o de Lolo para a prÛxima renderizaÁ„o
+		bgtz t3,LWR_INVALID		# Testa se √© um bloco and√°vel
+			savew(a1,LOLO_POSX)	# Atualiza a posi√ß√£o de Lolo para a pr√≥xima renderiza√ß√£o
 			jal IS_KEY_BLOCK
 			j LWR_INVALID_2
 	LWR_INVALID:
@@ -92,7 +92,7 @@ LOLO_WALK_RIGHT:
 	beqz t3,LWR_INVALID_2
 		jal PUSHABLE_RIGHT
 	LWR_INVALID_2:
-	jal COLISION_TEST			# Testa colis„o com inimigo
+	jal COLISION_TEST			# Testa colis√£o com inimigo
 	render_sprite(lolo_right_1, LOLO_POSX, LOLO_POSY)						
 	frame_refresh()					
 	j POLL_LOOP
@@ -102,16 +102,16 @@ LOLO_WALK_LEFT:
 	loadw(a1,LOLO_POSX)
 	addi a1,a1,-16
 	li t2, MAP_LEFT_EDGE
-	blt a1,t2, LWL_INVALID_2			# Testa se est· no mapa
+	blt a1,t2, LWL_INVALID_2			# Testa se est√° no mapa
 		loadw( t3,LOLO_POSY )
 		calculate_block(a1,t3)
-		jal IS_MORTAL_BLOCK		# Testa se È um bloco mortal
+		jal IS_MORTAL_BLOCK		# Testa se √© um bloco mortal
 		LWL_NOT_MORTAL:
 		la t2, WALKABLE_BLOCKS
 		add t3,t2,t1
 		lb t3,(t3)
-		bgtz t3,LWL_INVALID		# Testa se È um bloco and·vel
-			savew(a1,LOLO_POSX)	# Atualiza a posiÁ„o de Lolo para a prÛxima renderizaÁ„o
+		bgtz t3,LWL_INVALID		# Testa se √© um bloco and√°vel
+			savew(a1,LOLO_POSX)	# Atualiza a posi√ß√£o de Lolo para a pr√≥xima renderiza√ß√£o
 			jal IS_KEY_BLOCK
 			j LWL_INVALID_2
 	LWL_INVALID:
@@ -125,7 +125,7 @@ LOLO_WALK_LEFT:
 	j POLL_LOOP
 	
 #################################
-# Verifica se È um bloco mortal #
+# Verifica se √© um bloco mortal #
 #################################
 IS_MORTAL_BLOCK:
 	la t2, MORTAL_BLOCKS
@@ -151,13 +151,13 @@ IS_MORTAL_BLOCK:
 	NOT_MORTAL:
 	ret
 ################################
-# Verifica se È um bloco chave #
+# Verifica se √© um bloco chave #
 ################################
 IS_KEY_BLOCK:
 	la t2, KEY_BLOCKS
 	add t2,t2,t1
 	lb t3,(t2)
-	beqz t3, IKB_INVALID	# Testa se o bloco est· marcado como KEY no vetor KEY_BLOCKS
+	beqz t3, IKB_INVALID	# Testa se o bloco est√° marcado como KEY no vetor KEY_BLOCKS
 		li t3,0
 		sb t3,(t2)
 		loadw(t2,KEY_COUNTER)
@@ -172,13 +172,13 @@ IS_KEY_BLOCK:
 	ret
 
 #####################################
-# Verifica se È um bloco empurr·vel #
+# Verifica se √© um bloco empurr√°vel #
 #####################################
 IS_PUSHABLE_BLOCK:
 	la t2, PUSHABLE_BLOCKS
 	add t2,t2,t1
 	lb t3,(t2)
-	beqz t3, IPB_INVALID	# Testa se o bloco est· marcado como empurr·vel no vetor PUSHABLE_BLOCKS
+	beqz t3, IPB_INVALID	# Testa se o bloco est√° marcado como empurr√°vel no vetor PUSHABLE_BLOCKS
 		li t3,1
 		ret	
 	IPB_INVALID:
@@ -189,25 +189,25 @@ IS_PUSHABLE_BLOCK:
 	# Empurra para cima,baixo,direita ou esquerda #
 	###############################################
 	PUSHABLE_UP:
-	# a1 vem com a posiÁ„o Y do lolo atualizada (depois de andar)
+	# a1 vem com a posi√ß√£o Y do lolo atualizada (depois de andar)
 	loadw(a2,LOLO_POSX)
 	addi a3,a1,-16			# Calcula mais um passo pra cima
 	li t2,MAP_UPPER_EDGE
 	blt a3,t2,PU_INVALID
-	calculate_block(a2,a3)		# simulando um passo do bloco empurr·vel
+	calculate_block(a2,a3)		# simulando um passo do bloco empurr√°vel
 	la t2,WALKABLE_BLOCKS
 	add t2,t2,t1
 	lb t1,(t2)
-	beqz t1, PU_VALID		# Se o passo do bloco empurr·vel entrar em uma parede
-		PU_INVALID:		# ou em outro bloco marcado como n„o and·vel
-		calculate_block(a2,a1)	# Retorna ao loop 'lolo_walk' marcando o bloco empurr·vel
-		la t2,WALKABLE_BLOCKS	# como um bloco n„o and·vel
+	beqz t1, PU_VALID		# Se o passo do bloco empurr√°vel entrar em uma parede
+		PU_INVALID:		# ou em outro bloco marcado como n√£o and√°vel
+		calculate_block(a2,a1)	# Retorna ao loop 'lolo_walk' marcando o bloco empurr√°vel
+		la t2,WALKABLE_BLOCKS	# como um bloco n√£o and√°vel
 		add t2,t2,t1
 		li t1,1
 		sb t1,(t2)
 		ret
-	PU_VALID:			# Se n„o entrar em uma parede, executa as seguintes aÁıes:
-	savew(a1,LOLO_POSY)		# Salva a posiÁ„o atualizada do Lolo
+	PU_VALID:			# Se n√£o entrar em uma parede, executa as seguintes a√ß√µes:
+	savew(a1,LOLO_POSY)		# Salva a posi√ß√£o atualizada do Lolo
 	
 	loadw(a1,LOLO_POSX)		
 	loadw(a2,LOLO_POSY)	
@@ -219,16 +219,16 @@ IS_PUSHABLE_BLOCK:
 	li t1,0
 	sb t1,(t2)
 	sb t1,(t3)
-	addi a2,a2,-16			# Simula um passo do bloco empurr·vel
+	addi a2,a2,-16			# Simula um passo do bloco empurr√°vel
 	savew(a2,TEMP)
 	mv s7,ra
-	render_abs_sprite(tijolo,LOLO_POSX,LOLO_POSY)	# Renderiza um tijolo aonde o bloco empurr·vel estava
-	render_abs_sprite(pblock,LOLO_POSX,TEMP)	# Renderiza um bloco empurr·vel na proxima posiÁ„o
+	render_abs_sprite(tijolo,LOLO_POSX,LOLO_POSY)	# Renderiza um tijolo aonde o bloco empurr√°vel estava
+	render_abs_sprite(pblock,LOLO_POSX,TEMP)	# Renderiza um bloco empurr√°vel na proxima posi√ß√£o
 	loadw(a1,LOLO_POSX)
 	loadw(a2,TEMP)
-	calculate_block(a1,a2)		# Calcula o bloco em que o bloco empurr·vel est· agora
+	calculate_block(a1,a2)		# Calcula o bloco em que o bloco empurr√°vel est√° agora
 	la t2,PUSHABLE_BLOCKS
-	la t3,WALKABLE_BLOCKS		# Marca este bloco como empurr·vel E n„o and·vel
+	la t3,WALKABLE_BLOCKS		# Marca este bloco como empurr√°vel E n√£o and√°vel
 	add t2,t2,t1
 	add t3,t3,t1
 	li t1,1
@@ -403,16 +403,16 @@ ERASE_CURRENT_BLOCK:
 	ret
 
 ################################
-# Testa se o bloco È uma porta #
+# Testa se o bloco √© uma porta #
 ################################
 IS_DOOR:
 	li t1, DOOR_POSX
 	li t2, DOOR_POSY		
-	bne t2,a1,ID_FALSE			# Se a coordenada Y de lolo e da porta n„o coincidem, pula
+	bne t2,a1,ID_FALSE			# Se a coordenada Y de lolo e da porta n√£o coincidem, pula
 		loadw( t3,LOLO_POSX )
 		bne t3,t1,ID_FALSE		# Se coincidem, testa a coordenada X
 			loadw(t1,DOOR_STATE)
-			bnez t1, ID_FALSE 	# Testa se a porta est· aberta (state = 1)
+			bnez t1, ID_FALSE 	# Testa se a porta est√° aberta (state = 1)
 				savew( a1,LOLO_POSY )
 				render_sprite(lolo_coca, LOLO_POSX, LOLO_POSY)
 				frame_refresh()						
